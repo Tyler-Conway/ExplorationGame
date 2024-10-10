71ce5c1764c64f14a712f5e679a5207233e8c90a
Tyler-Conway <163177604+Tyler-Conway@users.noreply.github.com>
2024-10-10 12:16:36 -0700

    update

--- Exploration Game/src/main/UI.java
+++ Exploration Game/src/main/UI.java
@@ -1012,7 +1012,7 @@ public class UI {
 			}
 			
 			g2.setFont(g2.getFont().deriveFont(16F));
-			text = "(Press Up, Down, W, S, ESC, and Enter to Navigate)";
+			text = "(Press W, A, S, D, ESC, and Enter to Navigate)";
 			x = getXForCenteredText(text);
 			y += gp.tileSize;
 			g2.drawString(text, x, y);
